f43baabaac147ac0c389da9c00b4ed6e9594eeb9
Ayfri <pierre.ayfri@gmail.com>
2022-11-18 16:19:13 +0100

    feat: Add more helpers to generate NbtTags.

--- src/main/kotlin/arguments/Nbt.kt
+++ src/main/kotlin/arguments/Nbt.kt
@@ -2,16 +2,27 @@ package arguments
 
 import functions.Function
 import kotlinx.serialization.encodeToString
+import net.benwoodworth.knbt.NbtCompound
 import net.benwoodworth.knbt.NbtCompoundBuilder
+import net.benwoodworth.knbt.NbtListBuilder
 import net.benwoodworth.knbt.NbtTag
 import net.benwoodworth.knbt.StringifiedNbt
 import net.benwoodworth.knbt.buildNbtCompound
+import net.benwoodworth.knbt.buildNbtList
 import net.benwoodworth.knbt.put
 import net.benwoodworth.knbt.putNbtCompound
 
-fun Function.nbt(block: NbtCompoundBuilder.() -> Unit) = buildNbtCompound(block)
+fun nbt(block: NbtCompoundBuilder.() -> Unit) = buildNbtCompound(block)
+fun <T : NbtTag> nbtList(block: NbtListBuilder<T>.() -> Unit) = buildNbtList(block)
+
+@JvmName("nbtListCompound")
+fun nbtList(block: NbtListBuilder<NbtCompound>.() -> Unit) = buildNbtList(block)
+
 fun NbtCompoundBuilder.nbt(name: String, block: NbtCompoundBuilder.() -> Unit) = putNbtCompound(name, block)
 fun NbtCompoundBuilder.json(name: String, block: NbtCompoundBuilder.() -> Unit) = put(name, StringifiedNbt.encodeToString(buildNbtCompound(block)))
+
+fun stringifiedNbt(block: NbtCompoundBuilder.() -> Unit) = StringifiedNbt.encodeToString(buildNbtCompound(block))
+fun stringifiedNbtList(block: NbtListBuilder<NbtCompound>.() -> Unit) = StringifiedNbt.encodeToString(buildNbtList(block))
 
 fun Function.nbt(nbt: NbtTag) = literal(StringifiedNbt.encodeToString(nbt))
 
