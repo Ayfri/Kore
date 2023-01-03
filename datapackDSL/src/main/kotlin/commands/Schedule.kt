ad40403a7dc93bdaae12206114d4bc1c57e807b3
Ayfri <pierre.ayfri@gmail.com>
2023-01-04 00:11:16 +0100

    fix(schedule): Fix schedule syntax, add DSL with schedule name.

--- datapackDSL/src/main/kotlin/commands/Schedule.kt
+++ datapackDSL/src/main/kotlin/commands/Schedule.kt
@@ -5,25 +5,18 @@ import arguments.numbers.TimeNumber
 import arguments.time
 import functions.Function
 
+class ScheduleFunction(private val fn: Function, val function: String) {
+	fun append(time: TimeNumber) = fn.addLine(command("schedule", literal("function"), literal(function), time(time)))
+	fun clear() = fn.addLine(command("schedule", literal("clear"), literal(function)))
+	fun replace(time: TimeNumber) = fn.addLine(command("schedule", literal("function"), literal(function), time(time)))
+}
+
 class Schedule(private val fn: Function) {
-	fun append(function: String, time: TimeNumber) = fn.addLine(
-		command(
-			"schedule", literal("function"), literal(function), time(time), literal("append")
-		)
-	)
-	
-	fun clear(function: String) = fn.addLine(
-		command(
-			"schedule", literal("function"), literal(function), literal("clear")
-		)
-	)
-	
-	fun replace(function: String, time: TimeNumber) = fn.addLine(
-		command(
-			"schedule", literal("function"), literal(function), time(time), literal("replace")
-		)
-	)
+	fun append(function: String, time: TimeNumber) = fn.addLine(command("schedule", literal("function"), literal(function), time(time), literal("append")))
+	fun clear(function: String) = fn.addLine(command("schedule", literal("clear"), literal(function)))
+	fun replace(function: String, time: TimeNumber) = fn.addLine(command("schedule", literal("function"), literal(function), time(time), literal("replace")))
 }
 
 val Function.schedules get() = Schedule(this)
 fun Function.schedules(block: Schedule.() -> Command) = Schedule(this).block()
+fun Function.schedule(function: String) = ScheduleFunction(this, function)
