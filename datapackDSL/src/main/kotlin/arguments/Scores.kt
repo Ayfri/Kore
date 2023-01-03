2f0807ace87aced64037a2a569d0c01b5d51209e
Ayfri <pierre.ayfri@gmail.com>
2023-01-03 23:47:54 +0100

    fix(score): Fix scores in target selector serialization.

--- datapackDSL/src/main/kotlin/arguments/Scores.kt
+++ datapackDSL/src/main/kotlin/arguments/Scores.kt
@@ -8,29 +8,29 @@ import serializers.ToStringSerializer
 
 @Serializable(Score.Companion.ScoreSerializer::class)
 data class Score(val name: String, val value: IntRangeOrInt) {
-	override fun toString() = "$name=${value}"
-	
+	override fun toString() = "$name=$value"
+
 	companion object {
 		object ScoreSerializer : ToStringSerializer<Score>()
 	}
 }
 
 @Serializable(Scores.Companion.ScoresSerializer::class)
-data class Scores(val scores: Map<String, Score> = emptyMap()) {
-	override fun toString() = scores.entries.joinToString(",", "{", "}") { "${it.key}=${it.value}" }
-	
+data class Scores(val scores: Set<Score> = emptySet()) {
+	override fun toString() = scores.joinToString(",", "{", "}") { it.toString() }
+
 	companion object {
 		object ScoresSerializer : ToStringSerializer<Scores>()
 	}
 }
 
 class ScoreBuilder {
-	private val scores = mutableMapOf<String, Score>()
-	
-	fun score(name: String, value: IntRangeOrInt) = scores.put(name, Score(name, value))
-	fun score(name: String, value: Int) = scores.put(name, Score(name, value.asRangeOrInt()))
-	fun score(name: String, value: IntRange) = scores.put(name, Score(name, value.asRangeOrInt()))
-	
+	private val scores = mutableSetOf<Score>()
+
+	fun score(name: String, value: IntRangeOrInt) = scores.add(Score(name, value))
+	fun score(name: String, value: Int) = scores.add(Score(name, value.asRangeOrInt()))
+	fun score(name: String, value: IntRange) = scores.add(Score(name, value.asRangeOrInt()))
+
 	fun build() = Scores(scores)
 }
 
