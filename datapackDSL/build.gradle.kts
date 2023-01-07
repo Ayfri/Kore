80b9bec2fb8c1d544c2362ae095816a732c949a2
Ayfri <pierre.ayfri@gmail.com>
2023-01-07 01:41:13 +0100

    chore(kotlin): Add context receivers for dsl module.

--- datapackDSL/build.gradle.kts
+++ datapackDSL/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
 	kotlin("jvm") version "1.8.0"
 	kotlin("plugin.serialization") version "1.8.0"
@@ -12,6 +14,12 @@ dependencies {
 	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 	implementation(kotlin("reflect"))
 	api("net.benwoodworth.knbt:knbt:0.11.3")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xcontext-receivers")
+	}
 }
 
 application {
