699c6b778b622458949c9f55e4ba2846d11157d6
Ayfri <pierre.ayfri@gmail.com>
2023-02-22 02:47:20 +0100

    fix(dimension): Fix serialization of dimensions.

--- datapackDSL/src/main/kotlin/arguments/enums/Dimension.kt
+++ datapackDSL/src/main/kotlin/arguments/enums/Dimension.kt
@@ -1,17 +1,28 @@
 package arguments.enums
 
+import arguments.Argument
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.encoding.Encoder
 import serializers.LowercaseSerializer
 
 @Serializable(Dimension.Companion.DimensionSerializer::class)
-enum class Dimension {
+enum class Dimension : Argument.Dimension {
 	OVERWORLD,
 	THE_NETHER,
 	THE_END;
-	
+
+	override val namespace = "minecraft"
+
+	override fun asString() = "$namespace:${name.lowercase()}"
+
 	companion object {
 		val values = values()
-		
-		object DimensionSerializer : LowercaseSerializer<Dimension>(values)
+
+		object DimensionSerializer : LowercaseSerializer<Dimension>(values) {
+			override fun serialize(encoder: Encoder, value: Dimension) {
+				println("DimensionSerializer.serialize")
+				encoder.encodeString("minecraft:${value.name.lowercase()}")
+			}
+		}
 	}
 }
