59b997bc65b0ef212541c08c73efad5a3cf4c962
Ayfri <pierre.ayfri@gmail.com>
2023-03-27 04:53:01 +0200

    fix(gamerule): Add missing overload for querying the value of an Int Gamerule.

--- datapackDSL/src/main/kotlin/commands/Commands.kt
+++ datapackDSL/src/main/kotlin/commands/Commands.kt
@@ -64,7 +64,7 @@ fun Function.gamemode(gamemode: Gamemode, target: Argument.Entity? = null) = add
 
 fun Function.gamerule(rule: String, value: Boolean? = null) = addLine(command("gamerule", literal(rule), bool(value)))
 fun Function.gamerule(rule: String, value: Int) = addLine(command("gamerule", literal(rule), int(value)))
-fun Function.gamerule(rule: Gamerules.Int, value: Int) = addLine(command("gamerule", literal(rule.asArg()), int(value)))
+fun Function.gamerule(rule: Gamerules.Int, value: Int? = null) = addLine(command("gamerule", literal(rule.asArg()), int(value)))
 fun Function.gamerule(rule: Gamerules.Boolean, value: Boolean? = null) = addLine(command("gamerule", literal(rule.asArg()), bool(value)))
 
 fun Function.give(target: Argument.Entity, item: Argument.Item, count: Int? = null) = addLine(command("give", target, item, int(count)))
