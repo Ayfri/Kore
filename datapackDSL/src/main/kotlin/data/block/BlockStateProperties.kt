ffafbf085ac30b0185f903c6f82bf5d0a199f601
Ayfri <pierre.ayfri@gmail.com>
2023-04-21 14:35:50 +0200

    fix(BlockStateProperties builder): Fix duplicate JVM name for putAll method.

--- datapackDSL/src/main/kotlin/data/block/BlockStateProperties.kt
+++ datapackDSL/src/main/kotlin/data/block/BlockStateProperties.kt
@@ -20,12 +20,22 @@ data class BlockStatePropertiesBuilder(val properties: MutableMap<String, String
 	fun clear() = properties.clear()
 	fun remove(key: String) = properties.remove(key)
 
+	@JvmName("putAllStrings")
 	fun putAll(vararg properties: Pair<String, String>) = this.properties.putAll(properties)
+
+	@JvmName("putAllNumbers")
 	fun putAll(vararg properties: Pair<String, Number>) = this.properties.putAll(properties.map { it.first to it.second.toString() })
+
+	@JvmName("putAllBooleans")
 	fun putAll(vararg properties: Pair<String, Boolean>) = this.properties.putAll(properties.map { it.first to it.second.toString() })
 
+	@JvmName("putAllStrings")
 	fun putAll(properties: Map<String, String>) = this.properties.putAll(properties)
+
+	@JvmName("putAllNumbers")
 	fun putAll(properties: Map<String, Number>) = this.properties.putAll(properties.map { it.key to it.value.toString() })
+
+	@JvmName("putAllBooleans")
 	fun putAll(properties: Map<String, Boolean>) = this.properties.putAll(properties.map { it.key to it.value.toString() })
 
 	fun putAll(properties: BlockStatePropertiesBuilder) = this.properties.putAll(properties.properties)
