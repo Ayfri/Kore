c6e8f1bda0efe4211d8bb2814e38629a632a2a56
Ayfri <pierre.ayfri@gmail.com>
2023-04-21 14:34:54 +0200

    fix(blockstate serialization): Fix BlockState name property not serialized as 'Name'.

--- datapackDSL/src/main/kotlin/data/block/BlockState.kt
+++ datapackDSL/src/main/kotlin/data/block/BlockState.kt
@@ -1,9 +1,11 @@
 package data.block
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class BlockState(
+	@SerialName("Name")
 	val name: String,
 	val properties: Map<String, String>,
 )
