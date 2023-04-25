35f53617255b4cba51eb8dc358189600ce756dce
Ayfri <pierre.ayfri@gmail.com>
2023-04-25 19:21:21 +0200

    fix(recipes): Fix serialization of recipes ingredients.

--- datapackDSL/src/main/kotlin/features/recipes/data/Ingredient.kt
+++ datapackDSL/src/main/kotlin/features/recipes/data/Ingredient.kt
@@ -1,13 +1,33 @@
 package features.recipes.data
 
 import arguments.Argument
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.encoding.encodeStructure
 import serializers.SingleOrMultiSerializer
 
 typealias Ingredients = @Serializable(with = SingleOrMultiSerializer::class) MutableList<Ingredient>
 
-@Serializable
+@Serializable(with = Ingredient.Companion.IngredientSerializer::class)
 data class Ingredient(
 	var item: Argument.Item? = null,
 	var tag: Argument.ItemTag? = null,
-)
+) {
+	companion object {
+		@OptIn(ExperimentalSerializationApi::class)
+		@Serializer(forClass = Ingredient::class)
+		object DefaultIngredientSerializer
+
+		object IngredientSerializer : KSerializer<Ingredient> by DefaultIngredientSerializer {
+			override fun serialize(encoder: Encoder, value: Ingredient) {
+				encoder.encodeStructure(descriptor) {
+					value.item?.let { encodeStringElement(descriptor, 0, "${it.namespace}:${it.name.lowercase()}") }
+					value.tag?.let { encodeStringElement(descriptor, 1, "${it.namespace}:${it.name.lowercase()}") }
+				}
+			}
+		}
+	}
+}
