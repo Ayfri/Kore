9d28967d50d55fdc2b17cadd4d87f5f8fc43d4a0
Ayfri <pierre.ayfri@gmail.com>
2023-08-04 00:54:40 +0200

    refactor(display-slots): Update display slots names.

--- datapackDSL/src/main/kotlin/arguments/DisplaySlots.kt
+++ datapackDSL/src/main/kotlin/arguments/DisplaySlots.kt
@@ -5,7 +5,7 @@ import arguments.colors.NamedColor
 fun interface DisplaySlot : Argument
 
 object DisplaySlots {
-	val belowName = DisplaySlot { "belowName" }
+	val belowName = DisplaySlot { "below_name" }
 	val list = DisplaySlot { "list" }
 	val sidebar = DisplaySlot { "sidebar" }
 	fun sidebarTeam(color: NamedColor) = DisplaySlot { "sidebar.team.${color.name}" }
