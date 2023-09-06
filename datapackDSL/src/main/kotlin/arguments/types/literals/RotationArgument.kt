9e3e736773f594dba99709b4e7e0aa87ae2c5f7a
Ayfri <pierre.ayfri@gmail.com>
2023-09-06 13:36:10 +0200

    refactor(rotation): Make rotation call without arguments generate local rotation.

--- datapackDSL/src/main/kotlin/arguments/types/literals/RotationArgument.kt
+++ datapackDSL/src/main/kotlin/arguments/types/literals/RotationArgument.kt
@@ -2,6 +2,7 @@ package arguments.types.literals
 
 import arguments.Argument
 import arguments.numbers.RotNumber
+import arguments.numbers.relativeRot
 import arguments.numbers.rot
 import kotlinx.serialization.Serializable
 
@@ -10,5 +11,5 @@ data class RotationArgument(val yaw: RotNumber, val pitch: RotNumber) : Argument
 	override fun asString() = "$yaw $pitch"
 }
 
-fun rotation(yaw: Number = 0, pitch: Number = 0) = RotationArgument(yaw.rot, pitch.rot)
-fun rotation(hor: RotNumber, ver: RotNumber) = RotationArgument(hor, ver)
+fun rotation(yaw: Number, pitch: Number) = RotationArgument(yaw.rot, pitch.rot)
+fun rotation(hor: RotNumber = 0.relativeRot, ver: RotNumber = 0.relativeRot) = RotationArgument(hor, ver)
