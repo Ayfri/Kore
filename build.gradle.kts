bbab7a115aa22993c4f764ae34f77d4741349473
Ayfri <pierre.ayfri@gmail.com>
2023-12-02 23:40:49 +0100

    chore(publishing): Use nexus publish-plugin to automatically close staging repository on publish task.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,22 @@
 plugins {
 	kotlin("jvm") apply false
+	alias(libs.plugins.nexus.publish)
+}
+
+nexusPublishing {
+	repositories {
+		sonatype {
+			nexusUrl = uri(Project.PUBLISH_URL)
+			snapshotRepositoryUrl = uri(Project.SNAPSHOT_PUBLISH_URL)
+			username = System.getenv("NEXUS_USER") ?: return@sonatype
+			password = System.getenv("NEXUS_PASSWORD") ?: return@sonatype
+		}
+	}
+}
+
+val initializeSonatypeStagingRepository by tasks.existing
+subprojects {
+	initializeSonatypeStagingRepository {
+		shouldRunAfter(tasks.withType<Sign>())
+	}
 }
