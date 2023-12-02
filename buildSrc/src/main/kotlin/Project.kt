1d52072c934049a2c1a74c00135b380b743b38af
Ayfri <pierre.ayfri@gmail.com>
2023-12-03 00:14:19 +0100

    chore(publishing): Fix publish url variable.

--- buildSrc/src/main/kotlin/Project.kt
+++ buildSrc/src/main/kotlin/Project.kt
@@ -1,6 +1,6 @@
 object Project {
 	const val GROUP = "io.github.ayfri.kore"
-	const val PUBLISH_URL = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+	const val PUBLISH_URL = "https://s01.oss.sonatype.org/service/local/"
 	const val SNAPSHOT_PUBLISH_URL = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
 	const val URL = "github.com/Ayfri/Kore"
 	const val VERSION = "1.1.0"
