ab6de1c17b83b5b7968630dcfdb50990f9d4cc86
Ayfri <pierre.ayfri@gmail.com>
2024-03-29 15:17:16 +0100

    refactor(predicates): Simplify predicates serialization.

--- kore/src/main/kotlin/io/github/ayfri/kore/features/predicates/Predicate.kt
+++ kore/src/main/kotlin/io/github/ayfri/kore/features/predicates/Predicate.kt
@@ -5,19 +5,17 @@ import io.github.ayfri.kore.Generator
 import io.github.ayfri.kore.arguments.types.resources.PredicateArgument
 import io.github.ayfri.kore.features.predicates.conditions.PredicateCondition
 import io.github.ayfri.kore.serializers.InlinableList
-import io.github.ayfri.kore.serializers.inlinableListSerializer
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.descriptors.buildClassSerialDescriptor
 import kotlinx.serialization.encodeToString
-import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.serializer
 
 typealias PredicateAsList = @Serializable(Predicate.Companion.PredicateAsListSerializer::class) Predicate
 
-@Serializable(with = Predicate.Companion.PredicateSerializer::class)
+@Serializable
 data class Predicate(
 	override var fileName: String = "predicate",
 	var predicateConditions: InlinableList<PredicateCondition> = emptyList(),
@@ -25,20 +23,7 @@ data class Predicate(
 	override fun generateJson(dataPack: DataPack) = dataPack.jsonEncoder.encodeToString(predicateConditions)
 
 	companion object {
-		object PredicateSerializer : KSerializer<Predicate> {
-			override val descriptor = buildClassSerialDescriptor("Predicate")
-
-			override fun deserialize(decoder: Decoder) = error("Predicate cannot be deserialized")
-
-			override fun serialize(encoder: Encoder, value: Predicate) {
-				encoder.encodeSerializableValue(
-					inlinableListSerializer(serializer<PredicateCondition>()),
-					value.predicateConditions
-				)
-			}
-		}
-
-		object PredicateAsListSerializer : KSerializer<Predicate> by PredicateSerializer {
+		object PredicateAsListSerializer : KSerializer<Predicate> by serializer() {
 			override val descriptor = buildClassSerialDescriptor("PredicateAsList")
 
 			override fun serialize(encoder: Encoder, value: Predicate) {
