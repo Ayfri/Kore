1cda224ee14a282c69bc2b42b524997b3a82fb8c
Ayfri <pierre.ayfri@gmail.com>
2024-03-30 15:22:54 +0100

    fix(components): Fix components names.

--- kore/src/main/kotlin/io/github/ayfri/kore/arguments/components/LodestoneTargetComponent.kt
+++ kore/src/main/kotlin/io/github/ayfri/kore/arguments/components/LodestoneTargetComponent.kt
@@ -3,8 +3,8 @@ package io.github.ayfri.kore.arguments.components
 import io.github.ayfri.kore.arguments.maths.Vec3
 import io.github.ayfri.kore.arguments.types.resources.worldgen.DimensionArgument
 import io.github.ayfri.kore.generated.ComponentTypes
-import kotlin.math.roundToInt
 import kotlinx.serialization.Serializable
+import kotlin.math.roundToInt
 
 @Serializable
 data class LodestoneTargetComponent(
@@ -14,13 +14,13 @@ data class LodestoneTargetComponent(
 ) : Component()
 
 fun Components.lodestoneTarget(pos: List<Int>, dimension: DimensionArgument, tracked: Boolean? = null) = apply {
-	this[ComponentTypes.LODESTONE_TARGET] = LodestoneTargetComponent(pos, dimension, tracked)
+	this[ComponentTypes.LODESTONE_TRACKER] = LodestoneTargetComponent(pos, dimension, tracked)
 }
 
 fun Components.lodestoneTarget(x: Int, y: Int, z: Int, dimension: DimensionArgument, tracked: Boolean? = null) = apply {
-	this[ComponentTypes.LODESTONE_TARGET] = LodestoneTargetComponent(listOf(x, y, z), dimension, tracked)
+	this[ComponentTypes.LODESTONE_TRACKER] = LodestoneTargetComponent(listOf(x, y, z), dimension, tracked)
 }
 
 fun Components.lodestoneTarget(pos: Vec3, dimension: DimensionArgument, tracked: Boolean? = null) = apply {
-	this[ComponentTypes.LODESTONE_TARGET] = LodestoneTargetComponent(pos.values.map { it.value.roundToInt() }, dimension, tracked)
+	this[ComponentTypes.LODESTONE_TRACKER] = LodestoneTargetComponent(pos.values.map { it.value.roundToInt() }, dimension, tracked)
 }
