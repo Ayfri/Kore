5ac6cc7b866ce2f9ccc0f011d6cb3c6f60ba2153
Ayfri <pierre.ayfri@gmail.com>
2024-05-09 16:53:23 +0200

    chore(project): Increase project version to 1.6.0.

--- buildSrc/src/main/kotlin/Project.kt
+++ buildSrc/src/main/kotlin/Project.kt
@@ -3,5 +3,5 @@ object Project {
 	const val PUBLISH_URL = "https://s01.oss.sonatype.org/service/local/"
 	const val SNAPSHOT_PUBLISH_URL = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
 	const val URL = "github.com/Ayfri/Kore"
-	const val VERSION = "1.5.0"
+	const val VERSION = "1.6.0"
 }
