cfd9e55262289ecdb85792b1b5de0762f041d90a
Ayfri <pierre.ayfri@gmail.com>
2024-05-12 05:36:56 +0200

    refactor(providers): Make number providers functions not inlined, add function for storage number provider.

--- kore/src/main/kotlin/io/github/ayfri/kore/features/predicates/providers/functions.kt
+++ kore/src/main/kotlin/io/github/ayfri/kore/features/predicates/providers/functions.kt
@@ -4,21 +4,23 @@ package io.github.ayfri.kore.features.predicates.providers
 
 import io.github.ayfri.kore.features.predicates.types.EntityType
 
-inline fun constant(value: Float) = ConstantNumberProvider(value)
+fun constant(value: Float) = ConstantNumberProvider(value)
 
-inline fun uniform(min: NumberProvider, max: NumberProvider) = UniformNumberProvider(min, max)
-inline fun uniform(min: Float, max: Float) = UniformNumberProvider(constant(min), constant(max))
-inline fun uniform(min: Float, max: NumberProvider) = UniformNumberProvider(constant(min), max)
-inline fun uniform(min: NumberProvider, max: Float) = UniformNumberProvider(min, constant(max))
+fun uniform(min: NumberProvider, max: NumberProvider) = UniformNumberProvider(min, max)
+fun uniform(min: Float, max: Float) = UniformNumberProvider(constant(min), constant(max))
+fun uniform(min: Float, max: NumberProvider) = UniformNumberProvider(constant(min), max)
+fun uniform(min: NumberProvider, max: Float) = UniformNumberProvider(min, constant(max))
 
-
-inline fun binomial(n: NumberProvider, p: NumberProvider) = BinomialNumberProvider(n, p)
-inline fun binomial(n: Float, p: Float) = BinomialNumberProvider(constant(n), constant(p))
-inline fun binomial(n: Float, p: NumberProvider) = BinomialNumberProvider(constant(n), p)
-inline fun binomial(n: NumberProvider, p: Float) = BinomialNumberProvider(n, constant(p))
+fun binomial(n: NumberProvider, p: NumberProvider) = BinomialNumberProvider(n, p)
+fun binomial(n: Float, p: Float) = BinomialNumberProvider(constant(n), constant(p))
+fun binomial(n: Float, p: NumberProvider) = BinomialNumberProvider(constant(n), p)
+fun binomial(n: NumberProvider, p: Float) = BinomialNumberProvider(n, constant(p))
 
 fun scoreNumber(score: String, target: EntityType? = null, scale: Float? = null) =
 	ScoreNumberProvider(ScoreTargetNumberProvider(ScoreTargetType.CONTEXT, target = target), score, scale)
 
 fun scoreNumber(score: String, name: String? = null, scale: Float? = null) =
 	ScoreNumberProvider(ScoreTargetNumberProvider(ScoreTargetType.FIXED, name), score, scale)
+
+fun storageNumber(storage: String, path: String) =
+	StorageNumberProvider(storage, path)
