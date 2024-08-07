0f17edc50fc75bff491cacca5705b1e25b7c8101
Ayfri <pierre.ayfri@gmail.com>
2024-08-07 03:15:34 +0200

    refactor(predicates): Make `RandomChance` predicate condition use a number provider instead of float.

--- kore/src/main/kotlin/io/github/ayfri/kore/features/predicates/conditions/RandomChance.kt
+++ kore/src/main/kotlin/io/github/ayfri/kore/features/predicates/conditions/RandomChance.kt
@@ -1,13 +1,19 @@
 package io.github.ayfri.kore.features.predicates.conditions
 
 import io.github.ayfri.kore.features.predicates.Predicate
+import io.github.ayfri.kore.features.predicates.providers.NumberProvider
+import io.github.ayfri.kore.features.predicates.providers.constant
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class RandomChance(
-	var chance: Float,
+	var chance: NumberProvider,
 ) : PredicateCondition()
 
 fun Predicate.randomChance(chance: Float) {
+	predicateConditions += RandomChance(constant(chance))
+}
+
+fun Predicate.randomChance(chance: NumberProvider) {
 	predicateConditions += RandomChance(chance)
 }
