38a9ad0eb07bdd4a7120879c3a30cabddefd0849
Ayfri <pierre.ayfri@gmail.com>
2024-08-11 17:48:35 +0200

    refactor(tags): Standardize file names in `commonTags.kt` for consistency, make all them singular.

--- kore/src/main/kotlin/io/github/ayfri/kore/features/tags/commonTags.kt
+++ kore/src/main/kotlin/io/github/ayfri/kore/features/tags/commonTags.kt
@@ -4,113 +4,113 @@ import io.github.ayfri.kore.DataPack
 import io.github.ayfri.kore.arguments.types.resources.tagged.*
 
 fun DataPack.bannerPatternTag(
-	fileName: String = "banner_patterns",
+	fileName: String = "banner_pattern",
 	namespace: String = name,
 	replace: Boolean = false,
 	block: Tag<BannerPatternTagArgument>.() -> Unit = {},
-) = tag(fileName, "banner_patterns", namespace, replace, block)
+) = tag(fileName, "banner_pattern", namespace, replace, block)
 
 fun DataPack.biomeTag(
-	fileName: String = "biomes",
+	fileName: String = "biome",
 	namespace: String = name,
 	replace: Boolean = false,
 	block: Tag<BiomeTagArgument>.() -> Unit = {},
-) = tag(fileName, "biomes", namespace, replace, block)
+) = tag(fileName, "biome", namespace, replace, block)
 
 fun DataPack.blockTag(
-	fileName: String = "blocks",
+	fileName: String = "block",
 	namespace: String = name,
 	replace: Boolean = false,
 	block: Tag<BlockTagArgument>.() -> Unit = {},
-) = tag(fileName, "blocks", namespace, replace, block)
+) = tag(fileName, "block", namespace, replace, block)
 
 fun DataPack.catVariantTag(
-	fileName: String = "cat_variants",
+	fileName: String = "cat_variant",
 	namespace: String = name,
 	replace: Boolean = false,
 	block: Tag<CatVariantTagArgument>.() -> Unit = {},
-) = tag(fileName, "cat_variants", namespace, replace, block)
+) = tag(fileName, "cat_variant", namespace, replace, block)
 
 fun DataPack.damageTypeTag(
-	fileName: String = "damage_types",
+	fileName: String = "damage_type",
 	namespace: String = name,
 	replace: Boolean = false,
 	block: Tag<DamageTypeTagArgument>.() -> Unit = {},
-) = tag(fileName, "damage_types", namespace, replace, block)
+) = tag(fileName, "damage_type", namespace, replace, block)
 
 fun DataPack.entityTypeTag(
-	fileName: String = "entity_types",
+	fileName: String = "entity_type",
 	namespace: String = name,
 	replace: Boolean = false,
 	block: Tag<EntityTypeTagArgument>.() -> Unit = {},
-) = tag(fileName, "entity_types", namespace, replace, block)
+) = tag(fileName, "entity_type", namespace, replace, block)
 
 fun DataPack.flatLevelGeneratorPresetTag(
-	fileName: String = "flat_level_generator_presets",
+	fileName: String = "flat_level_generator_preset",
 	namespace: String = name,
 	replace: Boolean = false,
 	block: Tag<FlatLevelGeneratorPresetTagArgument>.() -> Unit = {},
-) = tag(fileName, "flat_level_generator_presets", namespace, replace, block)
+) = tag(fileName, "flat_level_generator_preset", namespace, replace, block)
 
 fun DataPack.fluidTag(
-	fileName: String = "fluids",
+	fileName: String = "fluid",
 	namespace: String = name,
 	replace: Boolean = false,
 	block: Tag<FluidTagArgument>.() -> Unit = {},
-) = tag(fileName, "fluids", namespace, replace, block)
+) = tag(fileName, "fluid", namespace, replace, block)
 
 fun DataPack.functionTag(
-	fileName: String = "io/github/ayfri/kore/functions/ayfri/kore/functions",
+	fileName: String = "function",
 	namespace: String = name,
 	replace: Boolean = false,
 	block: Tag<FunctionTagArgument>.() -> Unit = {},
-) = tag(fileName, "io/github/ayfri/kore/functions/ayfri/kore/functions", namespace, replace, block)
-
-fun DataPack.instrumentTag(
-	fileName: String = "instruments",
-	namespace: String = name,
-	replace: Boolean = false,
-	block: Tag<InstrumentTagArgument>.() -> Unit = {},
-) = tag(fileName, "instruments", namespace, replace, block)
-
-fun DataPack.itemTag(
-	fileName: String = "items",
-	namespace: String = name,
-	replace: Boolean = false,
-	block: Tag<ItemTagArgument>.() -> Unit = {},
-) = tag(fileName, "items", namespace, replace, block)
+) = tag(fileName, "function", namespace, replace, block)
 
 fun DataPack.gameEventTag(
-	fileName: String = "game_events",
+	fileName: String = "game_event",
 	namespace: String = name,
 	replace: Boolean = false,
 	block: Tag<GameEventTagArgument>.() -> Unit = {},
-) = tag(fileName, "game_events", namespace, replace, block)
+) = tag(fileName, "game_event", namespace, replace, block)
+
+fun DataPack.instrumentTag(
+	fileName: String = "instrument",
+	namespace: String = name,
+	replace: Boolean = false,
+	block: Tag<InstrumentTagArgument>.() -> Unit = {},
+) = tag(fileName, "instrument", namespace, replace, block)
+
+fun DataPack.itemTag(
+	fileName: String = "item",
+	namespace: String = name,
+	replace: Boolean = false,
+	block: Tag<ItemTagArgument>.() -> Unit = {},
+) = tag(fileName, "item", namespace, replace, block)
 
 fun DataPack.paintingVariantTag(
-	fileName: String = "painting_variants",
+	fileName: String = "painting_variant",
 	namespace: String = name,
 	replace: Boolean = false,
 	block: Tag<PaintingVariantTagArgument>.() -> Unit = {},
-) = tag(fileName, "painting_variants", namespace, replace, block)
+) = tag(fileName, "painting_variant", namespace, replace, block)
 
 fun DataPack.pointOfInterestTypeTag(
-	fileName: String = "point_of_interest_types",
+	fileName: String = "point_of_interest_type",
 	namespace: String = name,
 	replace: Boolean = false,
 	block: Tag<PointOfInterestTagArgument>.() -> Unit = {},
-) = tag(fileName, "point_of_interest_types", namespace, replace, block)
+) = tag(fileName, "point_of_interest_type", namespace, replace, block)
 
 fun DataPack.structureTag(
-	fileName: String = "structures",
+	fileName: String = "structure",
 	namespace: String = name,
 	replace: Boolean = false,
 	block: Tag<StructureTagArgument>.() -> Unit = {},
-) = tag(fileName, "structures", namespace, replace, block)
+) = tag(fileName, "structure", namespace, replace, block)
 
 fun DataPack.worldPresetTag(
-	fileName: String = "world_presets",
+	fileName: String = "world_preset",
 	namespace: String = name,
 	replace: Boolean = false,
 	block: Tag<WorldPresetTagArgument>.() -> Unit = {},
-) = tag(fileName, "world_presets", namespace, replace, block)
+) = tag(fileName, "world_preset", namespace, replace, block)
