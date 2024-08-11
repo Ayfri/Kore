a5de0c5d81a0eaa579e30a8a4c65ad2109959bdb
Ayfri <pierre.ayfri@gmail.com>
2024-08-11 18:19:04 +0200

    chore(build): Use dynamic `kotlin-version` from `gradle.properties`.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,4 +1,4 @@
-val kotlinVersion = "2.0.10"
+import java.util.*
 
 plugins {
 	`kotlin-dsl`
@@ -10,12 +10,17 @@ repositories {
 	gradlePluginPortal()
 }
 
+val props = Properties().apply {
+	file("../gradle.properties").inputStream().use { load(it) }
+}
+
+fun version(target: String): String = props.getProperty("$target.version")
+
 dependencies {
-	implementation(kotlin("gradle-plugin", kotlinVersion))
+	implementation(kotlin("gradle-plugin", version("kotlin")))
 	implementation(kotlin("script-runtime"))
 	implementation(kotlin("stdlib-jdk8"))
 }
-
 kotlin {
 	jvmToolchain(17)
 }
