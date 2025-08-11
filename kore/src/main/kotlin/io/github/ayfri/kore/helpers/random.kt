7772433aa1bbec9714ebafcf268d427c3ec5e8ce
Ayfri <pierre.ayfr@gmail.com>
2025-08-11 16:44:03 +0200

    docs(random): Add documentation for predicateRandomChance function, including usage example to clarify its purpose in executing commands with random chance.

--- kore/src/main/kotlin/io/github/ayfri/kore/helpers/random.kt
+++ kore/src/main/kotlin/io/github/ayfri/kore/helpers/random.kt
@@ -4,6 +4,22 @@ import io.github.ayfri.kore.DataPack
 import io.github.ayfri.kore.features.predicates.conditions.randomChance
 import io.github.ayfri.kore.features.predicates.predicate
 
+/**
+ * Creates a random chance predicate with the given chance.
+ * Usually used in [Function.execute] command to execute a command with a random chance.
+ *
+ * Example:
+ * ```kotlin
+ * execute {
+ *     ifCondition(predicateRandomChance(0.5))
+ *     run {
+ *         say("Hello")
+ *     }
+ * }
+ * ```
+ *
+ * Will create a predicate and reference the predicate in the execute command with a `random_chance` name.
+ */
 fun DataPack.predicateRandomChance(chance: Float, predicate: String = "random_chance") = predicate(predicate) {
 	randomChance(chance)
 }
