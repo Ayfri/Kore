b33c94b6cb82bd8d83055118faef86753759206a
Ayfri <pierre.ayfr@gmail.com>
2025-08-21 12:31:54 +0200

    feat(conditional-effect): Add support for vararg PredicateArgument in requirements method, and reference conditions.

--- kore/src/main/kotlin/io/github/ayfri/kore/features/enchantments/effects/builders/ConditionalEffect.kt
+++ kore/src/main/kotlin/io/github/ayfri/kore/features/enchantments/effects/builders/ConditionalEffect.kt
@@ -3,6 +3,8 @@ package io.github.ayfri.kore.features.enchantments.effects.builders
 import io.github.ayfri.kore.features.enchantments.effects.EnchantmentEffect
 import io.github.ayfri.kore.features.predicates.Predicate
 import io.github.ayfri.kore.features.predicates.conditions.PredicateCondition
+import io.github.ayfri.kore.features.predicates.conditions.reference
+import io.github.ayfri.kore.generated.arguments.types.PredicateArgument
 import io.github.ayfri.kore.serializers.InlinableList
 import kotlinx.serialization.Serializable
 
@@ -13,3 +15,9 @@ data class ConditionalEffect(
 )
 
 fun ConditionalEffect.requirements(block: Predicate.() -> Unit) = apply { requirements = Predicate().apply(block).predicateConditions }
+
+fun ConditionalEffect.requirements(vararg conditions: PredicateArgument) = apply {
+	requirements = Predicate().apply {
+		conditions.forEach { reference(it) }
+	}.predicateConditions
+}
