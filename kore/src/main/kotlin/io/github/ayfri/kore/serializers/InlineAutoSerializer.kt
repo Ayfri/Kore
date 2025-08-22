dd5facd3a2c0f31dd5bba6e5283e35dc6621a88f
Ayfri <pierre.ayfr@gmail.com>
2025-08-22 14:12:08 +0200

    refactor(serializers): Improve `InlineAutoSerializer` to handle specific serializer annotations, enhance serialization logic, adjust imports.

--- kore/src/main/kotlin/io/github/ayfri/kore/serializers/InlineAutoSerializer.kt
+++ kore/src/main/kotlin/io/github/ayfri/kore/serializers/InlineAutoSerializer.kt
@@ -1,14 +1,16 @@
 package io.github.ayfri.kore.serializers
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.serializer
-import kotlin.reflect.KClass
-import kotlin.reflect.KProperty1
-import kotlin.reflect.full.declaredMemberProperties
-import kotlin.reflect.full.primaryConstructor
 import net.benwoodworth.knbt.NbtTag
+import kotlin.reflect.KClass
+import kotlin.reflect.full.createInstance
+import kotlin.reflect.full.createType
+import kotlin.reflect.full.declaredMemberProperties
 
 /**
  * A serializer that serializes the first property of a class automatically.
@@ -23,18 +25,28 @@ import net.benwoodworth.knbt.NbtTag
  * data class MyDataClass(var myProperty: Int = 0)
  * ```
  */
-open class InlineAutoSerializer<T : Any>(klass: KClass<T>) : KSerializer<T> {
-	private val firstProperty: KProperty1<T, *> =
-		klass.declaredMemberProperties.firstOrNull()
-			?: error("No properties found for class ${klass.simpleName}")
-	private val propertySerializer: KSerializer<Any?> = @Suppress("UNCHECKED_CAST")
-	(serializer(firstProperty.returnType) as KSerializer<Any?>)
+open class InlineAutoSerializer<T : Any>(val klass: KClass<T>) : KSerializer<T> {
+	private val firstProperty =
+		klass.declaredMemberProperties.firstOrNull() ?: error("No properties found for class ${klass.simpleName}")
 
 	override val descriptor = NbtTag.serializer().descriptor
 
 	override fun deserialize(decoder: Decoder): T = error("InlineAutoSerializer cannot be deserialized")
 
+	@OptIn(ExperimentalSerializationApi::class)
 	override fun serialize(encoder: Encoder, value: T) {
-		encoder.encodeSerializableValue(propertySerializer, firstProperty.get(value))
+		val propertyDefaultSerializer = serializer(firstProperty.returnType)
+		val propertySpecifiedSerializerClass = firstProperty.annotations.filterIsInstance<Serializable>().firstOrNull()?.with
+		val propertySpecifiedSerializer = (propertySpecifiedSerializerClass?.objectInstance ?: propertySpecifiedSerializerClass?.createInstance()) as KSerializer<Any?>?
+		val propertyValue = firstProperty.get(value) ?: error("First property of ${klass.simpleName} is null")
+
+		val serializer= runCatching {
+			(
+				propertySpecifiedSerializer ?:
+				encoder.serializersModule.getContextual(propertyValue::class) ?:
+				encoder.serializersModule.serializer(propertyValue::class.createType())
+			) as KSerializer<Any?>
+		}
+		encoder.encodeSerializableValue(serializer.getOrDefault(propertyDefaultSerializer), propertyValue)
 	}
 }
