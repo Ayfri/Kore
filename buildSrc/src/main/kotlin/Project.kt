a3e782284806a18658f622796cc44df29b456c4f
Ayfri <pierre.ayfr@gmail.com>
2025-08-28 15:44:53 +0200

    chore(project): Increase project version to 1.27.3.

--- buildSrc/src/main/kotlin/Project.kt
+++ buildSrc/src/main/kotlin/Project.kt
@@ -3,7 +3,7 @@ import java.time.Year
 object Project {
 	const val GROUP = "io.github.ayfri.kore"
 	const val URL = "github.com/Ayfri/Kore"
-	const val VERSION = "1.27.2"
+	const val VERSION = "1.27.3"
 
 	// Publishing constants
 	const val CENTRAL_PORTAL_URL = "https://central.sonatype.com/api/v1/publisher"
