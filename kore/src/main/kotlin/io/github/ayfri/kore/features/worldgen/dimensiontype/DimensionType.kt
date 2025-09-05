157f7ca7fd02d4c5e20013d066cc50a96fc9d513
Ayfri <pierre.ayfr@gmail.com>
2025-09-05 09:46:57 +0200

    feat(dimension): Add optional cloudHeight property to DimensionType.

--- kore/src/main/kotlin/io/github/ayfri/kore/features/worldgen/dimensiontype/DimensionType.kt
+++ kore/src/main/kotlin/io/github/ayfri/kore/features/worldgen/dimensiontype/DimensionType.kt
@@ -44,6 +44,7 @@ data class DimensionType(
 	var height: Int = 16,
 	var monsterSpawnLightLevel: IntProvider = constant(0),
 	var monsterSpawnBlockLightLimit: Int = 0,
+	var cloudHeight: Int? = null,
 ) : Generator("dimension_type") {
 	@Transient
 	private lateinit var jsonEncoder: Json
--- kore/src/test/kotlin/io/github/ayfri/kore/features/worldgen/DimensionTypeTests.kt
+++ kore/src/test/kotlin/io/github/ayfri/kore/features/worldgen/DimensionTypeTests.kt
@@ -7,6 +7,7 @@ import io.github.ayfri.kore.features.worldgen.intproviders.*
 
 fun DataPack.dimensionTypeTests() {
 	dimensionType("my_dimension") {
+		cloudHeight = 128
 		height = 512
 		logicalHeight = 512
 		minY = 0
@@ -34,13 +35,35 @@ fun DataPack.dimensionTypeTests() {
 				"min_inclusive": 7,
 				"max_inclusive": 15
 			},
-			"monster_spawn_block_light_limit": 0
+			"monster_spawn_block_light_limit": 0,
+			"cloud_height": 128
 		}
 		""".trimIndent()
 
 	dimensionType("my_dimension2") {
 		monsterSpawnLightLevel = constant(2)
 	}
+
+	dimensionTypes.last() assertsIs """
+		{
+			"ultrawarm": false,
+			"natural": true,
+			"piglin_safe": false,
+			"respawn_anchor_works": false,
+			"bed_works": true,
+			"has_raids": true,
+			"has_skylight": true,
+			"has_ceiling": false,
+			"coordinate_scale": 1.0,
+			"ambient_light": 0.0,
+			"logical_height": 0,
+			"infiniburn": "#minecraft:infiniburn_overworld",
+			"min_y": 0,
+			"height": 16,
+			"monster_spawn_light_level": 2,
+			"monster_spawn_block_light_limit": 0
+		}
+	""".trimIndent()
 
 	dimensionType("my_dimension3") {
 		monsterSpawnLightLevel = weightedList {
