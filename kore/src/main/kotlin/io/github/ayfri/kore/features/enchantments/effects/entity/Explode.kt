444e2632122b549215aef46796b865a2d79f1c5f
Ayfri <pierre.ayfri@gmail.com>
2026-01-24 23:17:51 +0100

    feat(enchantments): Add `ExplodeBlockEffect` with particle effects, update `Explode` with block effects support, enhance `BlockInteraction` and particle handling.

--- kore/src/main/kotlin/io/github/ayfri/kore/features/enchantments/effects/entity/Explode.kt
+++ kore/src/main/kotlin/io/github/ayfri/kore/features/enchantments/effects/entity/Explode.kt
@@ -2,6 +2,8 @@ package io.github.ayfri.kore.features.enchantments.effects.entity
 
 import io.github.ayfri.kore.arguments.types.BlockOrTagArgument
 import io.github.ayfri.kore.arguments.types.resources.SoundArgument
+import io.github.ayfri.kore.features.enchantments.effects.entity.spawnparticles.types.ParticleType
+import io.github.ayfri.kore.features.enchantments.effects.entity.spawnparticles.types.particleType
 import io.github.ayfri.kore.features.enchantments.values.LevelBased
 import io.github.ayfri.kore.features.enchantments.values.constantLevelBased
 import io.github.ayfri.kore.generated.arguments.types.DamageTypeArgument
@@ -13,11 +15,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable(with = BlockInteraction.Companion.BlockInteractionSerializer::class)
 enum class BlockInteraction {
-	NONE,
 	BLOCK,
 	MOB,
+	NONE,
 	TNT,
-	TRIGGER;
+	TRIGGER,
+	;
 
 	companion object {
 		data object BlockInteractionSerializer : LowercaseSerializer<BlockInteraction>(entries)
@@ -25,19 +28,45 @@ enum class BlockInteraction {
 }
 
 @Serializable
+data class ExplodeBlockEffect(
+	var weight: Int,
+	var particle: ParticleType,
+	var scaling: Float? = null,
+	var speed: Float? = null,
+)
+
+@Serializable
 data class Explode(
 	var attributeToUser: Boolean,
+	var blockEffects: List<ExplodeBlockEffect>? = null,
+	var blockInteraction: BlockInteraction,
+	var createFire: Boolean,
 	var damageType: DamageTypeArgument? = null,
 	var immuneBlocks: InlinableList<BlockOrTagArgument>? = null,
 	var knockbackMultiplier: LevelBased? = null,
+	var largeParticle: ParticleTypeArgument,
 	var offset: TripleAsArray<Int, Int, Int>? = null,
 	var radius: LevelBased = constantLevelBased(0),
-	var createFire: Boolean,
-	var blockInteraction: BlockInteraction,
 	var smallParticle: ParticleTypeArgument,
-	var largeParticle: ParticleTypeArgument,
 	var sound: SoundArgument,
 ) : EntityEffect()
+
+fun Explode.blockEffects(block: MutableList<ExplodeBlockEffect>.() -> Unit) {
+	blockEffects = buildList(block)
+}
+
+fun MutableList<ExplodeBlockEffect>.effect(weight: Int, particle: ParticleType, scaling: Float? = null, speed: Float? = null) {
+	add(ExplodeBlockEffect(weight, particle, scaling, speed))
+}
+
+fun MutableList<ExplodeBlockEffect>.effect(
+	weight: Int,
+	particle: ParticleTypeArgument,
+	scaling: Float? = null,
+	speed: Float? = null,
+) {
+	add(ExplodeBlockEffect(weight, particleType(particle), scaling, speed))
+}
 
 fun Explode.damageType(value: DamageTypeArgument) {
 	damageType = value
