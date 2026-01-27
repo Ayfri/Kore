4a273d03d2bd0b776205ffbdb7b6ae325126f78a
Ayfri <pierre.ayfri@gmail.com>
2026-01-27 03:50:55 +0100

    test(serializers): Add tests for partial and full deserialization with `json` and `snbt` formats, ensure proper handling of optional and required fields.

--- kore/src/test/kotlin/io/github/ayfri/kore/serialization/Serializers.kt
+++ kore/src/test/kotlin/io/github/ayfri/kore/serialization/Serializers.kt
@@ -3,11 +3,11 @@ package io.github.ayfri.kore.serialization
 import io.github.ayfri.kore.assertions.assertsIsJson
 import io.github.ayfri.kore.assertions.assertsIsNbt
 import io.github.ayfri.kore.serializers.SinglePropertySimplifierSerializer
-import net.benwoodworth.knbt.ExperimentalNbtApi
-import net.benwoodworth.knbt.StringifiedNbt
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+import net.benwoodworth.knbt.ExperimentalNbtApi
+import net.benwoodworth.knbt.StringifiedNbt
 
 @OptIn(ExperimentalSerializationApi::class)
 val json = Json {
@@ -63,4 +63,34 @@ fun singlePropertySimplifierSerializer() {
 			optional: "optional"
 		}
 	""".trimIndent()
+
+	val decodedPartial = json.decodeFromString(Data.serializer(), "\"name\"")
+	assert(decodedPartial.name == "name")
+	assert(decodedPartial.optional == null)
+
+	val decodedFull = json.decodeFromString(
+		Data.serializer(), """
+		{
+			"name": "name",
+			"optional": "optional"
+		}
+	""".trimIndent()
+	)
+	assert(decodedFull.name == "name")
+	assert(decodedFull.optional == "optional")
+
+	val decodedPartialSnbt = snbt.decodeFromString(Data.serializer(), "\"name\"")
+	assert(decodedPartialSnbt.name == "name")
+	assert(decodedPartialSnbt.optional == null)
+
+	val decodedFullSnbt = snbt.decodeFromString(
+		Data.serializer(), """
+		{
+			name: "name",
+			optional: "optional"
+		}
+	""".trimIndent()
+	)
+	assert(decodedFullSnbt.name == "name")
+	assert(decodedFullSnbt.optional == "optional")
 }
