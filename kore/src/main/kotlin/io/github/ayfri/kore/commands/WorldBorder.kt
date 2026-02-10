105045b5623fb4e917e9705d35f18d82317a0711
Ayfri <pierre.ayfri@gmail.com>
2026-02-10 15:15:57 +0100

    feat(worldborder): Add support for `TimeNumber` in `add`, `set`, and `setWarningTime` methods.

--- kore/src/main/kotlin/io/github/ayfri/kore/commands/WorldBorder.kt
+++ kore/src/main/kotlin/io/github/ayfri/kore/commands/WorldBorder.kt
@@ -2,14 +2,17 @@ package io.github.ayfri.kore.commands
 
 import io.github.ayfri.kore.arguments.maths.Vec2
 import io.github.ayfri.kore.arguments.numbers.PosNumber
+import io.github.ayfri.kore.arguments.numbers.TimeNumber
 import io.github.ayfri.kore.arguments.types.literals.float
 import io.github.ayfri.kore.arguments.types.literals.int
 import io.github.ayfri.kore.arguments.types.literals.literal
+import io.github.ayfri.kore.arguments.types.literals.time
 import io.github.ayfri.kore.functions.Function
 import io.github.ayfri.kore.utils.asArg
 
 class WorldBorder(private val fn: Function) {
 	fun add(distance: Double, time: Int? = null) = fn.addLine(command("worldborder", literal("add"), float(distance), int(time)))
+	fun add(distance: Double, time: TimeNumber) = fn.addLine(command("worldborder", literal("add"), float(distance), time(time)))
 	fun center(pos: Vec2) = fn.addLine(command("center", pos))
 	fun center(x: Double, z: Double) = fn.addLine(command("worldborder", literal("center"), float(x), float(z)))
 	fun center(x: PosNumber, z: PosNumber) = fn.addLine(command("worldborder", literal("center"), literal(x.asArg()), literal(z.asArg())))
@@ -18,8 +21,10 @@ class WorldBorder(private val fn: Function) {
 	fun damagePerBlock(amount: Float) = damageAmount(amount)
 	fun get() = fn.addLine(command("worldborder", literal("get")))
 	fun set(distance: Double, time: Int? = null) = fn.addLine(command("worldborder", literal("set"), float(distance), int(time)))
+	fun set(distance: Double, time: TimeNumber) = fn.addLine(command("worldborder", literal("set"), float(distance), time(time)))
 	fun setWarningDistance(distance: Int) = fn.addLine(command("worldborder", literal("warning"), literal("distance"), int(distance)))
 	fun setWarningTime(time: Int) = fn.addLine(command("worldborder", literal("warning"), literal("time"), int(time)))
+	fun setWarningTime(time: TimeNumber) = fn.addLine(command("worldborder", literal("warning"), literal("time"), time(time)))
 }
 
 val Function.worldBorder get() = WorldBorder(this)
--- kore/src/test/kotlin/io/github/ayfri/kore/commands/WorldBorderCommand.kt
+++ kore/src/test/kotlin/io/github/ayfri/kore/commands/WorldBorderCommand.kt
@@ -1,12 +1,15 @@
 package io.github.ayfri.kore.commands
 
+import io.github.ayfri.kore.arguments.numbers.days
+import io.github.ayfri.kore.arguments.numbers.ticks
 import io.github.ayfri.kore.assertions.assertsIs
 import io.github.ayfri.kore.functions.Function
 
 fun Function.worldBorderTests() {
 	worldBorder {
 		add(1.0) assertsIs "worldborder add 1"
-		add(1.0, 5) assertsIs "worldborder add 1 5"
+		add(1.0, 2) assertsIs "worldborder add 1 2"
+		add(1.0, 5.days) assertsIs "worldborder add 1 5d"
 		center(1.0, 2.0) assertsIs "worldborder center 1 2"
 		damageAmount(1f) assertsIs "worldborder damage amount 1"
 		damageBuffer(1.0) assertsIs "worldborder damage buffer 1"
@@ -15,7 +18,7 @@ fun Function.worldBorderTests() {
 		set(1.0) assertsIs "worldborder set 1"
 		set(1.0, 5) assertsIs "worldborder set 1 5"
 		setWarningDistance(1) assertsIs "worldborder warning distance 1"
-		setWarningTime(1) assertsIs "worldborder warning time 1"
+		setWarningTime(1.ticks) assertsIs "worldborder warning time 1"
 	}
 
 	worldBorder.get() assertsIs "worldborder get"
