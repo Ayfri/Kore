60363c21d3720fdc4a0fd970341a98293edf0ee3
Ayfri <pierre.ayfri@gmail.com>
2026-02-12 23:36:18 +0100

    feat(predicates): Add `matchTool` function with `ItemArgument` support, enable multi-item condition handling.

--- kore/src/main/kotlin/io/github/ayfri/kore/features/predicates/conditions/MatchTool.kt
+++ kore/src/main/kotlin/io/github/ayfri/kore/features/predicates/conditions/MatchTool.kt
@@ -1,5 +1,6 @@
 package io.github.ayfri.kore.features.predicates.conditions
 
+import io.github.ayfri.kore.arguments.types.resources.ItemArgument
 import io.github.ayfri.kore.features.predicates.Predicate
 import io.github.ayfri.kore.features.predicates.sub.ItemStack
 import kotlinx.serialization.Serializable
@@ -8,6 +9,10 @@ import kotlinx.serialization.Serializable
 data class MatchTool(
 	var predicate: ItemStack,
 ) : PredicateCondition()
+
+fun Predicate.matchTool(vararg items: ItemArgument) {
+	predicateConditions += MatchTool(ItemStack(items = items.toList()))
+}
 
 fun Predicate.matchTool(predicate: ItemStack) {
 	predicateConditions += MatchTool(predicate)
