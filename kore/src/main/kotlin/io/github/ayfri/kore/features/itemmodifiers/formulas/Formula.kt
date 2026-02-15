e3a867fbfbf3c3b4334a99f6abad066457da4425
Ayfri <pierre.ayfri@gmail.com>
2026-02-15 16:50:42 +0100

    fix(item-modifiers): Fix serialization of `ApplyBonus` item modifier.

--- kore/src/main/kotlin/io/github/ayfri/kore/features/itemmodifiers/formulas/Formula.kt
+++ kore/src/main/kotlin/io/github/ayfri/kore/features/itemmodifiers/formulas/Formula.kt
@@ -1,7 +1,16 @@
 package io.github.ayfri.kore.features.itemmodifiers.formulas
 
+import io.github.ayfri.kore.serializers.NamespacedPolymorphicSerializer
 import kotlinx.serialization.Serializable
 
 /** Marker interface for `apply_bonus` formulas. */
-@Serializable
-sealed interface Formula
+@Serializable(with = Formula.Companion.FormulaSerializer::class)
+sealed interface Formula {
+	companion object {
+		data object FormulaSerializer : NamespacedPolymorphicSerializer<Formula>(
+			Formula::class,
+			moveIntoProperty = "parameters",
+			outputName = "formula"
+		)
+	}
+}
--- kore/src/main/kotlin/io/github/ayfri/kore/features/itemmodifiers/functions/ApplyBonus.kt
+++ kore/src/main/kotlin/io/github/ayfri/kore/features/itemmodifiers/functions/ApplyBonus.kt
@@ -4,7 +4,13 @@ import io.github.ayfri.kore.features.itemmodifiers.ItemModifier
 import io.github.ayfri.kore.features.itemmodifiers.formulas.Formula
 import io.github.ayfri.kore.features.predicates.PredicateAsList
 import io.github.ayfri.kore.generated.arguments.types.EnchantmentArgument
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.serialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.*
 
 /**
  * Applies a predefined bonus formula to the item stack count based on an enchantment level.
@@ -14,12 +20,38 @@ import kotlinx.serialization.Serializable
  * Docs: https://kore.ayfri.com/docs/data-driven/item-modifiers
  * See also: https://minecraft.wiki/w/Item_modifier
  */
-@Serializable
+@Serializable(with = ApplyBonus.Companion.ApplyBonusSerializer::class)
 data class ApplyBonus(
 	override var conditions: PredicateAsList? = null,
 	var enchantment: EnchantmentArgument,
 	var formula: Formula,
-) : ItemFunction()
+) : ItemFunction() {
+	companion object {
+		data object ApplyBonusSerializer : KSerializer<ApplyBonus> {
+			override val descriptor: SerialDescriptor = serialDescriptor<JsonElement>()
+
+			override fun deserialize(decoder: Decoder) = error("ApplyBonus cannot be deserialized")
+
+			override fun serialize(encoder: Encoder, value: ApplyBonus) {
+				require(encoder is JsonEncoder)
+				val json = encoder.json
+
+				val formulaJson = json.encodeToJsonElement(Formula.serializer(), value.formula).jsonObject
+
+				val result = buildJsonObject {
+					value.conditions?.let {
+						put("conditions", json.encodeToJsonElement(PredicateAsList.serializer(), it))
+					}
+					put("enchantment", json.encodeToJsonElement(value.enchantment))
+					formulaJson.forEach { (key, v) -> put(key, v) }
+				}
+
+				encoder.encodeJsonElement(result)
+			}
+		}
+
+	}
+}
 
 /** Add an `apply_bonus` step to this modifier. */
 fun ItemModifier.applyBonus(enchantment: EnchantmentArgument, formula: Formula, block: ApplyBonus.() -> Unit = {}) {
