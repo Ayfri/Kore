720a34dd5b58053cb387c51e8336334b73c7e423
Ayfri <pierre.ayfri@gmail.com>
2026-02-15 17:33:02 +0100

    test(item-modifiers): Add tests for all of item modifiers.

--- kore/src/test/kotlin/io/github/ayfri/kore/features/ItemModifierTests.kt
+++ kore/src/test/kotlin/io/github/ayfri/kore/features/ItemModifierTests.kt
@@ -3,11 +3,13 @@ package io.github.ayfri.kore.features
 import io.github.ayfri.kore.DataPack
 import io.github.ayfri.kore.arguments.WEAPON
 import io.github.ayfri.kore.arguments.colors.Color
+import io.github.ayfri.kore.arguments.colors.FormattingColor
 import io.github.ayfri.kore.arguments.components.data.EquipmentSlot
 import io.github.ayfri.kore.arguments.components.item.*
 import io.github.ayfri.kore.arguments.types.literals.self
 import io.github.ayfri.kore.assertions.assertsIs
 import io.github.ayfri.kore.commands.items
+import io.github.ayfri.kore.features.itemmodifiers.formulas.BinomialWithBonusCount
 import io.github.ayfri.kore.features.itemmodifiers.functions.*
 import io.github.ayfri.kore.features.itemmodifiers.itemModifier
 import io.github.ayfri.kore.features.itemmodifiers.types.Mode
@@ -22,53 +24,24 @@ import io.github.ayfri.kore.features.predicates.types.EntityType
 import io.github.ayfri.kore.functions.load
 import io.github.ayfri.kore.generated.*
 import io.github.ayfri.kore.generated.Enchantments
+import io.github.ayfri.kore.utils.set
 
 fun DataPack.itemModifierTests() {
-	val modifier = itemModifier("test_modifier") {
-		explorationMap {
-			decoration = MapDecorationTypes.BANNER_BLACK
-			destination = Tags.Worldgen.Structure.MINESHAFT
-			skipExistingChunks = true
-
-			conditions {
-				randomChance(0.5f)
-				weatherCheck(true)
-			}
-		}
-
-		setInstrument(Tags.Instrument.GOAT_HORNS)
+	itemModifier("apply_bonus") {
+		applyBonus(Enchantments.SHARPNESS, BinomialWithBonusCount(extra = 1, probability = 0.5f))
 	}
 
 	itemModifiers.last() assertsIs """
-		[
-			{
-				"function": "minecraft:exploration_map",
-				"conditions": [
-					{
-						"condition": "minecraft:random_chance",
-						"chance": 0.5
-					},
-					{
-						"condition": "minecraft:weather_check",
-						"raining": true
-					}
-				],
-				"destination": "minecraft:mineshaft",
-				"decoration": "minecraft:banner_black",
-				"skip_existing_chunks": true
-			},
-			{
-				"function": "minecraft:set_instrument",
-				"options": "#minecraft:goat_horns"
+		{
+			"function": "minecraft:apply_bonus",
+			"enchantment": "minecraft:sharpness",
+			"formula": "minecraft:binomial_with_bonus_count",
+			"parameters": {
+				"extra": 1,
+				"probability": 0.5
 			}
-		]
-	""".trimIndent()
-
-	load {
-		items {
-			modify(self(), WEAPON.MAINHAND, modifier)
 		}
-	}
+	""".trimIndent()
 
 	itemModifier("copy_components") {
 		copyComponents {
@@ -91,14 +64,64 @@ fun DataPack.itemModifierTests() {
 		}
 	""".trimIndent()
 
+	itemModifier("copy_custom_data") {
+		copyCustomData("kore:test") {
+			append("foo", "bar")
+			merge("baz", "qux")
+			replace("quux", "corge")
+		}
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:copy_custom_data",
+			"source": {
+				"type": "minecraft:storage",
+				"source": "kore:test"
+			},
+			"ops": [
+				{
+					"op": "append",
+					"source": "foo",
+					"target": "bar"
+				},
+				{
+					"op": "merge",
+					"source": "baz",
+					"target": "qux"
+				},
+				{
+					"op": "replace",
+					"source": "quux",
+					"target": "corge"
+				}
+			]
+		}
+	""".trimIndent()
+
 	itemModifier("copy_name") {
-		copyName(Source.LAST_DAMAGE_PLAYER)
+		copyName(Source.ATTACKING_PLAYER)
 	}
 
 	itemModifiers.last() assertsIs """
 		{
 			"function": "minecraft:copy_name",
-			"source": "last_damage_player"
+			"source": "attacking_player"
+		}
+	""".trimIndent()
+
+	itemModifier("copy_state") {
+		copyState(Blocks.OAK_LOG, "axis", "waterlogged")
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:copy_state",
+			"block": "minecraft:oak_log",
+			"properties": [
+				"axis",
+				"waterlogged"
+			]
 		}
 	""".trimIndent()
 
@@ -152,6 +175,48 @@ fun DataPack.itemModifierTests() {
 		}
 	""".trimIndent()
 
+	itemModifier("exploration_map") {
+		explorationMap {
+			decoration = MapDecorationTypes.FRAME
+			destination = Tags.Worldgen.Structure.MINESHAFT
+			searchRadius = 20
+			skipExistingChunks = true
+			zoom = 1
+		}
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:exploration_map",
+			"destination": "minecraft:mineshaft",
+			"decoration": "minecraft:frame",
+			"zoom": 1,
+			"search_radius": 20,
+			"skip_existing_chunks": true
+		}
+	""".trimIndent()
+
+	itemModifier("explosion_decay") {
+		explosionDecay()
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:explosion_decay"
+		}
+	""".trimIndent()
+
+	itemModifier("fill_player_head") {
+		fillPlayerHead(Source.THIS)
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:fill_player_head",
+			"entity": "this"
+		}
+	""".trimIndent()
+
 	itemModifier("filtered") {
 		filtered {
 			itemFilter(Items.APPLE)
@@ -182,6 +247,16 @@ fun DataPack.itemModifierTests() {
 		}
 	""".trimIndent()
 
+	itemModifier("furnace_smelt") {
+		furnaceSmelt()
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:furnace_smelt"
+		}
+	""".trimIndent()
+
 	itemModifier("limit_count") {
 		limitCount(
 			providersRange(
@@ -205,7 +280,7 @@ fun DataPack.itemModifierTests() {
 					"scale": 4.0
 				},
 				"max": {
-					"type": "minecraft:enchantment",
+					"type": "minecraft:enchantment_level",
 					"amount": 5
 				}
 			}
@@ -231,9 +306,47 @@ fun DataPack.itemModifierTests() {
 		}
 	""".trimIndent()
 
+	itemModifier("reference") {
+		reference("kore:my_modifier")
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:reference",
+			"name": "kore:my_modifier"
+		}
+	""".trimIndent()
+
+	itemModifier("sequence") {
+		sequence {
+			setItem(Items.APPLE)
+			setName("Test", color = Color.BLACK)
+		}
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:sequence",
+			"functions": [
+				{
+					"function": "minecraft:set_item",
+					"item": "minecraft:apple"
+				},
+				{
+					"function": "minecraft:set_name",
+					"name": {
+						"text": "Test",
+						"color": "black",
+						"type": "text"
+					}
+				}
+			]
+		}
+	""".trimIndent()
+
 	itemModifier("set_attributes") {
 		setAttributes {
-			attribute(Attributes.SCALE, 0.5f, slot = listOf(EquipmentSlot.MAINHAND))
+			attribute(Attributes.SCALE, "test", 0.5f, slot = listOf(EquipmentSlot.MAINHAND))
 			replace = false
 		}
 	}
@@ -245,6 +358,7 @@ fun DataPack.itemModifierTests() {
 				{
 					"attribute": "minecraft:scale",
 					"amount": 0.5,
+					"id": "test",
 					"operation": "add_value",
 					"slot": "mainhand"
 				}
@@ -253,7 +367,39 @@ fun DataPack.itemModifierTests() {
 		}
 	""".trimIndent()
 
-	itemModifier("set_components_modifier") {
+	itemModifier("set_banner_pattern") {
+		setBannerPattern(append = true) {
+			bannerPattern(BannerPatterns.CREEPER, FormattingColor.BLACK)
+		}
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:set_banner_pattern",
+			"patterns": [
+				{
+					"pattern": "minecraft:creeper",
+					"color": "black"
+				}
+			],
+			"append": true
+		}
+	""".trimIndent()
+
+	itemModifier("set_book_cover") {
+		setBookCover("Test Title", author = "Test Author", generation = 2)
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:set_book_cover",
+			"title": "\"Test Title\"",
+			"author": "Test Author",
+			"generation": 2
+		}
+	""".trimIndent()
+
+	itemModifier("set_components") {
 		setComponents {
 			customName("Test", color = Color.BLACK)
 			!damage(5)
@@ -298,6 +444,33 @@ fun DataPack.itemModifierTests() {
 					"weight": 1
 				}
 			]
+		}
+	""".trimIndent()
+
+	itemModifier("set_count") {
+		setCount(5f, add = true)
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:set_count",
+			"count": 5.0,
+			"add": true
+		}
+	""".trimIndent()
+
+	itemModifier("set_custom_data") {
+		setCustomData {
+			this["test"] = 1
+		}
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:set_custom_data",
+			"tag": {
+				"test": 1
+			}
 		}
 	""".trimIndent()
 
@@ -373,6 +546,58 @@ fun DataPack.itemModifierTests() {
 		}
 	""".trimIndent()
 
+	itemModifier("set_damage") {
+		setDamage(0.5f, add = true)
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:set_damage",
+			"damage": 0.5,
+			"add": true
+		}
+	""".trimIndent()
+
+	itemModifier("set_enchantments") {
+		setEnchantments(add = true) {
+			this[Enchantments.SHARPNESS] = constant(2f)
+		}
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:set_enchantments",
+			"enchantments": {
+				"minecraft:sharpness": 2.0
+			},
+			"add": true
+		}
+	""".trimIndent()
+
+	itemModifier("set_firework_explosion") {
+		setFireworkExplosion(FireworkExplosionShape.STAR) {
+			colors = listOf(Color.RED.toRGB())
+			fadeColors = listOf(Color.BLUE.toRGB())
+			hasFlicker = true
+			hasTrail = true
+		}
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:set_firework_explosion",
+			"shape": "star",
+			"colors": [
+				16733525
+			],
+			"fade_colors": [
+				5592575
+			],
+			"has_trail": true,
+			"has_flicker": true
+		}
+	""".trimIndent()
+
 	itemModifier("set_fireworks") {
 		setFireworks {
 			flightDuration = 5
@@ -412,6 +637,30 @@ fun DataPack.itemModifierTests() {
 		}
 	""".trimIndent()
 
+	itemModifier("set_item") {
+		setItem(Items.APPLE)
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:set_item",
+			"item": "minecraft:apple"
+		}
+	""".trimIndent()
+
+	itemModifier("set_loot_table") {
+		setLootTable(BlockEntityTypes.CHEST, LootTables.Chests.SIMPLE_DUNGEON, seed = 42)
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:set_loot_table",
+			"type": "minecraft:chest",
+			"name": "minecraft:chests/simple_dungeon",
+			"seed": 42
+		}
+	""".trimIndent()
+
 	itemModifier("set_lore") {
 		setLore {
 			lore("Test", color = Color.BLACK)
@@ -433,17 +682,6 @@ fun DataPack.itemModifierTests() {
 			],
 			"mode": "insert",
 			"offset": 0
-		}
-	""".trimIndent()
-
-	itemModifier("set_item") {
-		setItem(Items.APPLE)
-	}
-
-	itemModifiers.last() assertsIs """
-		{
-			"function": "minecraft:set_item",
-			"item": "minecraft:apple"
 		}
 	""".trimIndent()
 
@@ -478,6 +716,56 @@ fun DataPack.itemModifierTests() {
 		}
 	""".trimIndent()
 
+	itemModifier("set_potion") {
+		setPotion(Potions.HEALING)
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:set_potion",
+			"id": "minecraft:healing"
+		}
+	""".trimIndent()
+
+	itemModifier("set_stew_effect") {
+		setStewEffect {
+			potionEffect(Potions.NIGHT_VISION, constant(200f))
+		}
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:set_stew_effect",
+			"effects": [
+				{
+					"type": "minecraft:night_vision",
+					"duration": 200.0
+				}
+			]
+		}
+	""".trimIndent()
+
+	itemModifier("set_writable_book_pages") {
+		setWritableBookPages {
+			page("test", filtered = "filtered")
+			mode(Mode.INSERT, 1)
+		}
+	}
+
+	itemModifiers.last() assertsIs """
+		{
+			"function": "minecraft:set_writable_book_pages",
+			"pages": [
+				{
+					"raw": "test",
+					"filtered": "filtered"
+				}
+			],
+			"mode": "insert",
+			"offset": 1
+		}
+	""".trimIndent()
+
 	itemModifier("set_written_book_pages") {
 		setWrittenBookPages {
 			page("test", filtered = "test2")
@@ -496,6 +784,52 @@ fun DataPack.itemModifierTests() {
 			"mode": "replace_all"
 		}
 	""".trimIndent()
+
+	val modifier = itemModifier("test_modifier") {
+		explorationMap {
+			decoration = MapDecorationTypes.BANNER_BLACK
+			destination = Tags.Worldgen.Structure.MINESHAFT
+			skipExistingChunks = true
+
+			conditions {
+				randomChance(0.5f)
+				weatherCheck(true)
+			}
+		}
+
+		setInstrument(Tags.Instrument.GOAT_HORNS)
+	}
+
+	itemModifiers.last() assertsIs """
+		[
+			{
+				"function": "minecraft:exploration_map",
+				"conditions": [
+					{
+						"condition": "minecraft:random_chance",
+						"chance": 0.5
+					},
+					{
+						"condition": "minecraft:weather_check",
+						"raining": true
+					}
+				],
+				"destination": "minecraft:mineshaft",
+				"decoration": "minecraft:banner_black",
+				"skip_existing_chunks": true
+			},
+			{
+				"function": "minecraft:set_instrument",
+				"options": "#minecraft:goat_horns"
+			}
+		]
+	""".trimIndent()
+
+	load {
+		items {
+			modify(self(), WEAPON.MAINHAND, modifier)
+		}
+	}
 
 	itemModifier("toggle_tooltips") {
 		toggleTooltips {
