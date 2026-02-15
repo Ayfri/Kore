e9598accc328fefe294af3491b100e7863055639
Ayfri <pierre.ayfri@gmail.com>
2026-02-15 15:42:58 +0100

    test(advancements): Add comprehensive tests for all advancement triggers, remove `icon_components` test.

--- kore/src/test/kotlin/io/github/ayfri/kore/features/AdvancementTests.kt
+++ kore/src/test/kotlin/io/github/ayfri/kore/features/AdvancementTests.kt
@@ -33,7 +33,7 @@ fun DataPack.advancementTests() {
 		@Suppress("DEPRECATION")
 		criteria(
 			name = "test",
-			triggerCondition = ConsumeItem("test", item = itemStack(Items.ENCHANTED_GOLDEN_APPLE))
+			triggerCondition = ConsumeItem(item = itemStack(Items.ENCHANTED_GOLDEN_APPLE))
 		) {
 			randomChance(chance = 0.5f)
 			timeCheck(10f..20f)
@@ -128,37 +128,6 @@ fun DataPack.advancementTests() {
 		}
 	}
 
-	advancement("icon_components") {
-		display(Items.AIR, "Hello", "World") {
-			icon(Items.DIAMOND_SWORD, 10) {
-				enchantments {
-					enchantment(Enchantments.SHARPNESS, 5)
-				}
-			}
-		}
-	}
-
-	advancements.last() assertsIs """
-		{
-			"display": {
-				"icon": {
-					"id": "minecraft:diamond_sword",
-					"components": {
-						"enchantments": {
-							"minecraft:sharpness": 5
-						}
-					},
-					"count": 10
-				},
-				"title": "Hello",
-				"description": "World",
-				"frame": "task"
-			},
-			"criteria": {}
-		}
-	""".trimIndent()
-
-
 	val namespacedAdvancement = advancement("test_namespaced") {
 		namespace = "advancements_tests"
 	}
@@ -170,6 +139,158 @@ fun DataPack.advancementTests() {
 }
 
 private fun DataPack.allTriggersTests() {
+	advancement("allay_drop_item_on_block") {
+		criteria {
+			allayDropItemOnBlock("allay_drop_item_on_block") {
+				location {
+					block {
+						blocks(Blocks.STONE)
+					}
+				}
+			}
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"allay_drop_item_on_block": {
+					"trigger": "minecraft:allay_drop_item_on_block",
+					"conditions": {
+						"location": {
+							"block": {
+								"blocks": "minecraft:stone"
+							}
+						}
+					}
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("any_block_use") {
+		criteria {
+			anyBlockUse("any_block_use")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"any_block_use": {
+					"trigger": "minecraft:any_block_use"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("avoid_vibrations") {
+		criteria {
+			avoidVibrations("avoid_vibrations")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"avoid_vibrations": {
+					"trigger": "minecraft:avoid_vibrations"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("bee_nest_destroyed") {
+		criteria {
+			beeNestDestroyed("bee_nest_destroyed", block = Blocks.BEE_NEST, numBeesInside = rangeOrInt(1)) {}
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"bee_nest_destroyed": {
+					"trigger": "minecraft:bee_nest_destroyed",
+					"conditions": {
+						"block": "minecraft:bee_nest",
+						"num_bees_inside": 1
+					}
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("bred_animals") {
+		criteria {
+			bredAnimals("bred_animals")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"bred_animals": {
+					"trigger": "minecraft:bred_animals"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("brewed_potion") {
+		criteria {
+			brewedPotion("brewed_potion")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"brewed_potion": {
+					"trigger": "minecraft:brewed_potion"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("changed_dimension") {
+		criteria {
+			changedDimension("changed_dimension")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"changed_dimension": {
+					"trigger": "minecraft:changed_dimension"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("channeled_lightning") {
+		criteria {
+			channeledLightning("channeled_lightning")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"channeled_lightning": {
+					"trigger": "minecraft:channeled_lightning"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("construct_beacon") {
+		criteria {
+			constructBeacon("construct_beacon")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"construct_beacon": {
+					"trigger": "minecraft:construct_beacon"
+				}
+			}
+		}
+	""".trimIndent()
+
 	advancement("consume_item") {
 		criteria {
 			consumeItem("consume_item") {
@@ -218,10 +339,10 @@ private fun DataPack.allTriggersTests() {
 		}
 	""".trimIndent()
 
-	advancement("crafter_recipe_crafter") {
+	advancement("crafter_recipe_crafted") {
 		criteria {
 			crafterRecipeCrafted(
-				"crafter_recipe_crafter",
+				"crafter_recipe_crafted",
 				Recipes.POLISHED_BLACKSTONE_BRICK_STAIRS_FROM_POLISHED_BLACKSTONE_BRICKS_STONECUTTING
 			) {
 				conditions {
@@ -241,7 +362,7 @@ private fun DataPack.allTriggersTests() {
 	advancements.last() assertsIs """
 		{
 			"criteria": {
-				"crafter_recipe_crafter": {
+				"crafter_recipe_crafted": {
 					"trigger": "minecraft:crafter_recipe_crafted",
 					"conditions": {
 						"player": [
@@ -267,6 +388,36 @@ private fun DataPack.allTriggersTests() {
 						],
 						"recipe_id": "minecraft:polished_blackstone_brick_stairs_from_polished_blackstone_bricks_stonecutting"
 					}
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("cured_zombie_villager") {
+		criteria {
+			curedZombieVillager("cured_zombie_villager")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"cured_zombie_villager": {
+					"trigger": "minecraft:cured_zombie_villager"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("default_block_use") {
+		criteria {
+			defaultBlockUse("default_block_use")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"default_block_use": {
+					"trigger": "minecraft:default_block_use"
 				}
 			}
 		}
@@ -321,6 +472,21 @@ private fun DataPack.allTriggersTests() {
 		}
 	""".trimIndent()
 
+	advancement("enchanted_item") {
+		criteria {
+			enchantedItem("enchanted_item")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"enchanted_item": {
+					"trigger": "minecraft:enchanted_item"
+				}
+			}
+		}
+	""".trimIndent()
+
 	advancement("enter_block") {
 		criteria {
 			enterBlock("enter_block") {
@@ -342,6 +508,36 @@ private fun DataPack.allTriggersTests() {
 							"lit": "true"
 						}
 					}
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("entity_hurt_player") {
+		criteria {
+			entityHurtPlayer("entity_hurt_player")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"entity_hurt_player": {
+					"trigger": "minecraft:entity_hurt_player"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("entity_killed_player") {
+		criteria {
+			entityKilledPlayer("entity_killed_player")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"entity_killed_player": {
+					"trigger": "minecraft:entity_killed_player"
 				}
 			}
 		}
@@ -415,6 +611,126 @@ private fun DataPack.allTriggersTests() {
 		}
 	""".trimIndent()
 
+	advancement("fall_from_height") {
+		criteria {
+			fallFromHeight("fall_from_height")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"fall_from_height": {
+					"trigger": "minecraft:fall_from_height"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("filled_bucket") {
+		criteria {
+			filledBucket("filled_bucket")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"filled_bucket": {
+					"trigger": "minecraft:filled_bucket"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("fishing_rod_hooked") {
+		criteria {
+			fishingRodHooked("fishing_rod_hooked")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"fishing_rod_hooked": {
+					"trigger": "minecraft:fishing_rod_hooked"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("hero_of_the_village") {
+		criteria {
+			heroOfTheVillage("hero_of_the_village")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"hero_of_the_village": {
+					"trigger": "minecraft:hero_of_the_village"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("impossible") {
+		criteria {
+			impossible("impossible")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"impossible": {
+					"trigger": "minecraft:impossible"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("inventory_changed") {
+		criteria {
+			inventoryChanged("inventory_changed")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"inventory_changed": {
+					"trigger": "minecraft:inventory_changed"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("item_durability_changed") {
+		criteria {
+			itemDurabilityChanged("item_durability_changed")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"item_durability_changed": {
+					"trigger": "minecraft:item_durability_changed"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("item_used_on_block") {
+		criteria {
+			itemUsedOnBlock("item_used_on_block")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"item_used_on_block": {
+					"trigger": "minecraft:item_used_on_block"
+				}
+			}
+		}
+	""".trimIndent()
+
 	advancement("killed_by_arrow") {
 		criteria {
 			killedByArrow("killed_by_arrow") {
@@ -473,6 +789,144 @@ private fun DataPack.allTriggersTests() {
 		}
 	""".trimIndent()
 
+	advancement("kill_mob_near_sculk_catalyst") {
+		criteria {
+			killMobNearSculkCatalyst("kill_mob_near_sculk_catalyst")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"kill_mob_near_sculk_catalyst": {
+					"trigger": "minecraft:kill_mob_near_sculk_catalyst"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("levitation") {
+		criteria {
+			levitation("levitation")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"levitation": {
+					"trigger": "minecraft:levitation"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("lightning_strike") {
+		criteria {
+			lightningStrike("lightning_strike")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"lightning_strike": {
+					"trigger": "minecraft:lightning_strike"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("location") {
+		criteria {
+			location("location")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"location": {
+					"trigger": "minecraft:location"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("nether_travel") {
+		criteria {
+			netherTravel("nether_travel")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"nether_travel": {
+					"trigger": "minecraft:nether_travel"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("placed_block") {
+		criteria {
+			placedBlock("placed_block")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"placed_block": {
+					"trigger": "minecraft:placed_block"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("player_generates_container_loot") {
+		criteria {
+			playerGeneratesContainerLoot("player_generates_container_loot", LootTables.Chests.IGLOO_CHEST) {}
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"player_generates_container_loot": {
+					"trigger": "minecraft:player_generates_container_loot",
+					"conditions": {
+						"loot_table": "minecraft:chests/igloo_chest"
+					}
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("player_hurt_entity") {
+		criteria {
+			playerHurtEntity("player_hurt_entity")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"player_hurt_entity": {
+					"trigger": "minecraft:player_hurt_entity"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("player_killed_entity") {
+		criteria {
+			playerKilledEntity("player_killed_entity")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"player_killed_entity": {
+					"trigger": "minecraft:player_killed_entity"
+				}
+			}
+		}
+	""".trimIndent()
+
 	advancement("player_sheared_equipment") {
 		criteria {
 			playerShearedEquipment("player_sheared_equipment") {
@@ -503,6 +957,108 @@ private fun DataPack.allTriggersTests() {
 		}
 	""".trimIndent()
 
+	advancement("recipe_crafted") {
+		criteria {
+			recipeCrafted(
+				"recipe_crafted",
+				Recipes.POLISHED_BLACKSTONE_BRICK_STAIRS_FROM_POLISHED_BLACKSTONE_BRICKS_STONECUTTING
+			) {}
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"recipe_crafted": {
+					"trigger": "minecraft:recipe_crafted",
+					"conditions": {
+						"recipe_id": "minecraft:polished_blackstone_brick_stairs_from_polished_blackstone_bricks_stonecutting"
+					}
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("recipe_unlocked") {
+		criteria {
+			recipeUnlocked(
+				"recipe_unlocked",
+				Recipes.POLISHED_BLACKSTONE_BRICK_STAIRS_FROM_POLISHED_BLACKSTONE_BRICKS_STONECUTTING
+			) {}
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"recipe_unlocked": {
+					"trigger": "minecraft:recipe_unlocked",
+					"conditions": {
+						"recipe": "minecraft:polished_blackstone_brick_stairs_from_polished_blackstone_bricks_stonecutting"
+					}
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("ride_entity_in_lava") {
+		criteria {
+			rideEntityInLava("ride_entity_in_lava")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"ride_entity_in_lava": {
+					"trigger": "minecraft:ride_entity_in_lava"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("shot_crossbow") {
+		criteria {
+			shotCrossbow("shot_crossbow")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"shot_crossbow": {
+					"trigger": "minecraft:shot_crossbow"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("slept_in_bed") {
+		criteria {
+			sleptInBed("slept_in_bed")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"slept_in_bed": {
+					"trigger": "minecraft:slept_in_bed"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("slide_down_block") {
+		criteria {
+			slideDownBlock("slide_down_block") {}
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"slide_down_block": {
+					"trigger": "minecraft:slide_down_block"
+				}
+			}
+		}
+	""".trimIndent()
+
 	advancement("spear_mobs") {
 		criteria {
 			spearMobs("spear_mobs") {
@@ -518,6 +1074,186 @@ private fun DataPack.allTriggersTests() {
 					"conditions": {
 						"count": 3
 					}
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("started_riding") {
+		criteria {
+			startedRiding("started_riding")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"started_riding": {
+					"trigger": "minecraft:started_riding"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("summoned_entity") {
+		criteria {
+			summonedEntity("summoned_entity")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"summoned_entity": {
+					"trigger": "minecraft:summoned_entity"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("tame_animal") {
+		criteria {
+			tameAnimal("tame_animal")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"tame_animal": {
+					"trigger": "minecraft:tame_animal"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("target_hit") {
+		criteria {
+			targetHit("target_hit")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"target_hit": {
+					"trigger": "minecraft:target_hit"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("thrown_item_picked_up_by_entity") {
+		criteria {
+			thrownItemPickedUpByEntity("thrown_item_picked_up_by_entity")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"thrown_item_picked_up_by_entity": {
+					"trigger": "minecraft:thrown_item_picked_up_by_entity"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("thrown_item_picked_up_by_player") {
+		criteria {
+			thrownItemPickedUpByPlayer("thrown_item_picked_up_by_player")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"thrown_item_picked_up_by_player": {
+					"trigger": "minecraft:thrown_item_picked_up_by_player"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("tick") {
+		criteria {
+			tick("tick")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"tick": {
+					"trigger": "minecraft:tick"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("used_ender_eye") {
+		criteria {
+			usedEnderEye("used_ender_eye")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"used_ender_eye": {
+					"trigger": "minecraft:used_ender_eye"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("used_totem") {
+		criteria {
+			usedTotem("used_totem")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"used_totem": {
+					"trigger": "minecraft:used_totem"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("using_item") {
+		criteria {
+			usingItem("using_item")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"using_item": {
+					"trigger": "minecraft:using_item"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("villager_trade") {
+		criteria {
+			villagerTrade("villager_trade")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"villager_trade": {
+					"trigger": "minecraft:villager_trade"
+				}
+			}
+		}
+	""".trimIndent()
+
+	advancement("voluntary_exile") {
+		criteria {
+			voluntaryExile("voluntary_exile")
+		}
+	}
+	advancements.last() assertsIs """
+		{
+			"criteria": {
+				"voluntary_exile": {
+					"trigger": "minecraft:voluntary_exile"
 				}
 			}
 		}
