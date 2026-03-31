c7cdcfa8ad2b1ee3f489f950c62d8ff67acc0a72
Ayfri <pierre.ayfri@gmail.com>
2026-03-31 18:37:48 +0200

    fix(commands): Add missing `literal("under")` argument to `spreadPlayers` command.

--- kore/src/main/kotlin/io/github/ayfri/kore/commands/SpreadPlayers.kt
+++ kore/src/main/kotlin/io/github/ayfri/kore/commands/SpreadPlayers.kt
@@ -5,6 +5,7 @@ import io.github.ayfri.kore.arguments.types.EntityArgument
 import io.github.ayfri.kore.arguments.types.literals.bool
 import io.github.ayfri.kore.arguments.types.literals.float
 import io.github.ayfri.kore.arguments.types.literals.int
+import io.github.ayfri.kore.arguments.types.literals.literal
 import io.github.ayfri.kore.functions.Function
 
 fun Function.spreadPlayers(
@@ -22,4 +23,15 @@ fun Function.spreadPlayers(
 	maxHeight: Int,
 	respectTeams: Boolean,
 	targets: EntityArgument,
-) = addLine(command("spreadplayers", center, float(spreadDistance), float(maxRange), int(maxHeight), bool(respectTeams), targets))
+) = addLine(
+	command(
+		"spreadplayers",
+		center,
+		float(spreadDistance),
+		float(maxRange),
+		literal("under"),
+		int(maxHeight),
+		bool(respectTeams),
+		targets
+	)
+)
