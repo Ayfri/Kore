15b5437d0b1d4b9060b44a4ffa49fa4638657fb3
Ayfri <pierre.ayfri@gmail.com>
2026-04-15 12:21:01 +0200

    chore(kotest-conventions): Enhance test logging with full exception format, stack traces, and failure events.

--- build-logic/convention/src/main/kotlin/kotest-conventions.gradle.kts
+++ build-logic/convention/src/main/kotlin/kotest-conventions.gradle.kts
@@ -1,12 +1,20 @@
-// On chope le catalogue 'libs' de façon stricte
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
 val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
 dependencies {
-	// Le add() avec string bypass le besoin de l'accesseur testImplementation
 	add("testImplementation", libs.findLibrary("kotest-assertions-core").get())
 	add("testImplementation", libs.findLibrary("kotest-runner-junit5").get())
 }
 
 tasks.withType<Test>().configureEach {
 	useJUnitPlatform()
+
+	testLogging {
+		events("failed", "standardError")
+		exceptionFormat = TestExceptionFormat.FULL
+		showExceptions = true
+		showCauses = true
+		showStackTraces = true
+	}
 }
