e7d0d5a4b998f9b06e2b91ea63406008738debef
Ayfri <pierre.ayfri@gmail.com>
2026-06-26 13:11:51 +0200

    refactor(number-providers): Remove custom serializer from `ScoreTargetNumberProvider`, simplify implementation.

--- kore/src/main/kotlin/io/github/ayfri/kore/features/predicates/providers/NumberProviders.kt
+++ kore/src/main/kotlin/io/github/ayfri/kore/features/predicates/providers/NumberProviders.kt
@@ -2,17 +2,12 @@ package io.github.ayfri.kore.features.predicates.providers
 
 import io.github.ayfri.kore.features.enchantments.values.LevelBased
 import io.github.ayfri.kore.features.predicates.types.EntityType
-import io.github.ayfri.kore.generated.LootScoreProviderTypes
 import io.github.ayfri.kore.generated.arguments.types.EnvironmentAttributeArgument
 import io.github.ayfri.kore.generated.arguments.types.LootScoreProviderTypeArgument
 import io.github.ayfri.kore.serializers.InlineAutoSerializer
 import io.github.ayfri.kore.serializers.NamespacedPolymorphicSerializer
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.buildClassSerialDescriptor
-import kotlinx.serialization.descriptors.element
-import kotlinx.serialization.encoding.*
 
 @Serializable(with = NumberProvider.Companion.NumberProviderSerializer::class)
 sealed class NumberProvider {
@@ -74,55 +69,16 @@ data class EnvironmentAttributeNumberProvider(
 /**
  * Selects the player name or context entity for a [Score] query.
  *
- * @param type [LootScoreProviderTypes.FIXED] to use a literal player name/UUID, [LootScoreProviderTypes.CONTEXT] to use a loot context entity.
- * @param name Player name or UUID string; required when [type] is [LootScoreProviderTypes.FIXED].
- * @param target Loot context entity selector; required when [type] is [LootScoreProviderTypes.CONTEXT].
+ * @param type [io.github.ayfri.kore.generated.LootScoreProviderTypes.FIXED] to use a literal player name/UUID, [io.github.ayfri.kore.generated.LootScoreProviderTypes.CONTEXT] to use a loot context entity.
+ * @param name Player name or UUID string; required when [type] is [io.github.ayfri.kore.generated.LootScoreProviderTypes.FIXED].
+ * @param target Loot context entity selector; required when [type] is [io.github.ayfri.kore.generated.LootScoreProviderTypes.CONTEXT].
  */
-@Serializable(ScoreTargetNumberProvider.Companion.ScoreTargetNumberProviderSerializer::class)
+@Serializable
 data class ScoreTargetNumberProvider(
 	var type: LootScoreProviderTypeArgument,
 	var name: String? = null,
 	var target: EntityType? = null,
-) {
-	companion object {
-		data object ScoreTargetNumberProviderSerializer : KSerializer<ScoreTargetNumberProvider> {
-			override val descriptor = buildClassSerialDescriptor("ScoreTargetNumberProvider") {
-				element<String>("type")
-				element<String>("name")
-				element<String>("target")
-			}
-
-			override fun deserialize(decoder: Decoder) = decoder.decodeStructure(descriptor) {
-				var type: LootScoreProviderTypeArgument? = null
-				var name: String? = null
-				var target: EntityType? = null
-
-				while (true) {
-					when (val index = decodeElementIndex(descriptor)) {
-						0 -> type = decodeSerializableElement(descriptor, 0, LootScoreProviderTypeArgument.serializer())
-						1 -> name = decodeStringElement(descriptor, 1)
-						2 -> target = decodeSerializableElement(descriptor, 2, EntityType.serializer())
-						CompositeDecoder.DECODE_DONE -> break
-						else -> error("Unexpected index: $index")
-					}
-				}
-				requireNotNull(type) { "type is required" }
-				ScoreTargetNumberProvider(type, name, target)
-			}
-
-			override fun serialize(encoder: Encoder, value: ScoreTargetNumberProvider) {
-				encoder.encodeStructure(descriptor) {
-					encodeSerializableElement(descriptor, 0, LootScoreProviderTypeArgument.serializer(), value.type)
-
-					when (value.type) {
-						LootScoreProviderTypes.FIXED -> encodeStringElement(descriptor, 1, value.name!!)
-						else -> encodeSerializableElement(descriptor, 2, EntityType.serializer(), value.target!!)
-					}
-				}
-			}
-		}
-	}
-}
+)
 
 /**
  * Reads and optionally scales a scoreboard value.
