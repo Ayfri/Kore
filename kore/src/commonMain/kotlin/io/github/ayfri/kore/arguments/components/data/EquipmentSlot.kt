b5df734e07c60bc0257c913770c6eb12bf221092
Ayfri <pierre.ayfri@gmail.com>
2026-08-20 17:20:11 +0200

    fix(components): Add the missing `saddle` equipment slot.

--- kore/src/commonMain/kotlin/io/github/ayfri/kore/arguments/components/data/EquipmentSlot.kt
+++ kore/src/commonMain/kotlin/io/github/ayfri/kore/arguments/components/data/EquipmentSlot.kt
@@ -3,6 +3,11 @@ package io.github.ayfri.kore.arguments.components.data
 import io.github.ayfri.kore.serializers.LowercaseSerializer
 import kotlinx.serialization.Serializable
 
+/**
+ * A slot, or group of slots, an item can be equipped in.
+ *
+ * Minecraft Wiki: https://minecraft.wiki/w/Slot
+ */
 @Serializable(EquipmentSlot.Companion.EquipmentSlotSerializer::class)
 enum class EquipmentSlot {
 	ANY,
@@ -14,7 +19,8 @@ enum class EquipmentSlot {
 	LEGS,
 	FEET,
 	ARMOR,
-	BODY;
+	BODY,
+	SADDLE;
 
 	companion object {
 		data object EquipmentSlotSerializer : LowercaseSerializer<EquipmentSlot>(entries)
