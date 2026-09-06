b9a62878a25386af5e25746db56944f28593bd3d
Ayfri <pierre.ayfri@gmail.com>
2026-09-06 03:38:03 +0200

    chore(project): Increase project version to 2.14.0.

--- build-logic/convention/src/main/kotlin/Project.kt
+++ build-logic/convention/src/main/kotlin/Project.kt
@@ -2,7 +2,7 @@ import java.time.Year
 
 data object Project {
 	const val GROUP = "io.github.ayfri.kore"
-	const val VERSION = "2.13.1"
+	const val VERSION = "2.14.0"
 
 	// URLS
 	const val GITHUB_URL = "github.com/Ayfri/Kore"
